72959e07e68364150815bb177adade76ad425da2
Ben Hughes <bhughes.me@gmail.com>
2021-03-21 16:17:50 +0000

    fixed issue by moving scope

--- src/PreferenceManager.ts
+++ src/PreferenceManager.ts
@@ -1,66 +1,63 @@
+(() => {
+  class PreferenceManager implements IPreferenceManager {
+    preferences: Preferences;
+    config: PreferenceManagerConfig;
+    constructor(name: string, config: PreferenceManagerConfig) {
+      this.preferences = new Preferences(name);
+      this.config = config;
+    }
+    async editPreferences() {
+      var inputForm = new Form();
 
+      for (const configItemId of Object.keys(this.config.items)) {
+        const configItem = this.config.items[configItemId];
 
+        switch (configItem.type) {
+          case 'string': {
+            const currentValue = this.getPreference(configItem.id);
+            const taskNameField = new Form.Field.String(
+              configItem.id,
+              configItem.displayName,
+              currentValue,
+              null,
+            );
+            inputForm.addField(taskNameField, null);
+            break;
+          }
+          case 'boolean': {
+            const currentValue = this.preferences.readBoolean(configItem.id);
+            const taskNameField = new Form.Field.Checkbox(
+              configItem.id,
+              configItem.displayName,
+              currentValue,
+            );
+            inputForm.addField(taskNameField, null);
+            break;
+          }
 
-class PreferenceManager implements IPreferenceManager{
-  preferences: Preferences;
-  config: PreferenceManagerConfig;
-  constructor(name: string, config: PreferenceManagerConfig) {
-    this.preferences = new Preferences(name);
-    this.config = config;
-  }
-  async editPreferences() {
-    var inputForm = new Form();
-
-    for (const configItemId of Object.keys(this.config.items)) {
-      const configItem = this.config.items[configItemId];
-
-      switch (configItem.type) {
-        case 'string': {
-          const currentValue = this.getPreference(configItem.id);
-          const taskNameField = new Form.Field.String(
-            configItem.id,
-            configItem.displayName,
-            currentValue,
-            null,
-          );
-          inputForm.addField(taskNameField, null);
-          break;
+          default:
+            break;
         }
-        case 'boolean': {
-          const currentValue = this.preferences.readBoolean(configItem.id);
-          const taskNameField = new Form.Field.Checkbox(
-            configItem.id,
-            configItem.displayName,
-            currentValue,
-          );
-          inputForm.addField(taskNameField, null);
-          break;
-        }
-
-        default:
-          break;
+      }
+      const result = await inputForm.show(
+        this.config?.editPreferences?.dialogText || '',
+        this.config?.editPreferences?.confirmTitle || 'Save',
+      );
+      const fromValues = result.values as Record<string, any>;
+      for (const configItemId of Object.keys(this.config.items)) {
+        const configItem = this.config.items[configItemId];
+        const newValue = fromValues[configItem.id];
+        this.preferences.write(configItem.id, newValue);
       }
     }
-    const result = await inputForm.show(
-      this.config?.editPreferences?.dialogText || '',
-      this.config?.editPreferences?.confirmTitle || 'Save',
-    );
-    const fromValues = result.values as Record<string, any>;
-    for (const configItemId of Object.keys(this.config.items)) {
-      const configItem = this.config.items[configItemId];
-      const newValue = fromValues[configItem.id];
-      this.preferences.write(configItem.id, newValue);
+
+    getPreference(id: string) {
+      const prefValue = this.preferences.read(id);
+
+      return prefValue !== null ? prefValue : this.config.items[id].default;
     }
   }
 
-  getPreference(id: string) {
-    const prefValue = this.preferences.read(id);
-
-    return prefValue !== null ? prefValue : this.config.items[id].default;
-  }
-}
-
-(() => {
   const config: PreferenceManagerConfig = {
     editPreferences: {
       dialogText:
@@ -79,7 +76,7 @@ class PreferenceManager implements IPreferenceManager{
         type: 'string',
         default: 'working-on',
       },
-        namePrefix: {
+      namePrefix: {
         id: 'namePrefix',
         displayName: 'Prefix for currently working on task',
         type: 'string',
--- src/common.ts
+++ src/common.ts
@@ -1,29 +1,29 @@
-const common: ICommon = {
-  log: async function log(message: string, title = 'Log') {
-    const a = new Alert(title, message);
-    a.addOption('OK');
-    return await a.show(null);
-  },
-
-  resetTasks: function (trackingName: string, trackingNamePrefix: string) {
-    let trackingTag: Tag = flattenedTags.find(
-      (t) => t.name === trackingName,
-    );
-
-    if (!trackingTag) {
-      trackingTag = new Tag(trackingName, null);
-    }
-
-    trackingTag.tasks.forEach((task) => {
-      if (task.name.startsWith(trackingNamePrefix)) {
-        task.name = task.name.replace(trackingNamePrefix, '');
-      }
-      task.removeTag(trackingTag);
-    });
-  },
-};
-
 (() => {
+  const common: ICommon = {
+    log: async function log(message: string, title = 'Log') {
+      const a = new Alert(title, message);
+      a.addOption('OK');
+      return await a.show(null);
+    },
+
+    resetTasks: function (trackingName: string, trackingNamePrefix: string) {
+      let trackingTag: Tag = flattenedTags.find(
+        (t) => t.name === trackingName,
+      );
+
+      if (!trackingTag) {
+        trackingTag = new Tag(trackingName, null);
+      }
+
+      trackingTag.tasks.forEach((task) => {
+        if (task.name.startsWith(trackingNamePrefix)) {
+          task.name = task.name.replace(trackingNamePrefix, '');
+        }
+        task.removeTag(trackingTag);
+      });
+    },
+  };
+
   const lib = new PlugIn.Library(new Version('1.0'));
 
   // @ts-ignore
